0f744f4de553818b36f2d4f25efe39897fc551fe
Juan Luis Romero <juanlu_21@hotamil.com>
2017-05-28 19:05:15 +0200

    3 commit. Problemas al confirmar email válido

--- src/com/Juanlu/Email/ValidateEmail.java
+++ src/com/Juanlu/Email/ValidateEmail.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by juanl on 26/05/2017.
@@ -33,8 +35,43 @@ class Capa extends JPanel{
         JTextField cajaTexto = new JTextField(30);
         JButton boton = new JButton("Validar");
 
+        boton.addActionListener(new AccionBoton(cajaTexto.getText()));
+
         add(etiqueta);
         add(cajaTexto);
         add(boton);
+
+    }
+
+    class AccionBoton implements ActionListener{
+        String email;
+
+        public AccionBoton(String e){
+            email = e;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e){
+            if (isEmail(email)){
+                System.out.println("El email es valido");
+            }else{
+                System.out.println("El email es invalido");
+            }
+        }
+        private boolean isEmail(String correo) {
+            Pattern pat = null;
+            Matcher mat = null;
+            pat = Pattern.compile("^([0-9a-zA-Z]([_.w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-w]*[0-9a-zA-Z].)+([a-zA-Z]{2,9}.)+[a-zA-Z]{2,3})$");
+            mat = pat.matcher(correo);
+            if (mat.find()) {
+                System.out.println("[" + mat.group() + "]");
+                return true;
+            }else{
+                return false;
+            }
+        }
+
     }
 }
+
+
